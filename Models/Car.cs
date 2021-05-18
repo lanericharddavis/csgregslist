b71096b8f0d0097162405428a9789dd35450e7ba
Lane <lanericharddavis@gmail.com>
2021-05-18 15:42:55 -0600

    required added

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,13 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace csgregslist.Models
 {
   public class Car
   {
     public string Id { get; set; }
+
+    [Required]
     public string Make { get; set; }
+
+    [Required]
     public string Model { get; set; }
+
+    [Required]
     public int Year { get; set; }
+
+    [Required]
     public int Price { get; set; }
     public string Description { get; set; }
     // public string Image { get; set; }
